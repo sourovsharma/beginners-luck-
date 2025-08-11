ef62a988c7a4fea780814ceb184bf440948e3eee
sourovsharma <83174622+sourovsharma@users.noreply.github.com>
2025-08-11 16:40:58 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,40 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor# Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+# 3.  Displays the results of each operation on the screen.
+
+
+# num1=int(input("enter first number: "))
+# num2=int(input("enter second number: "))
+# op=int(input("enter\n 1 for addition\n 2 for subtraction\n 3 for multiplication\n 4 for division\n>>"))
+# if op==1:
+#     sum=num1+num2
+#     print(sum)
+# elif op==2:
+#     sub=num1-num2
+#     print(sub)
+# elif op==3:
+#     mult=num1*num2
+#     print(mult)
+# elif op==4:
+#     div=num1/num2
+#     print(div)
+# else:
+#     print("invalid input")
+
+#OG
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+add=num1+num2
+print("addition - ",add)
+sub=num1-num2
+print("subtract - ",sub)
+mult=num1*num2
+print("multiplication - ",mult)
+div=num1/num2
+print("division - ",div)
---
+++ q2.py
@@ -0,0 +1,10 @@
+# Task 2: Create a Personalized Greeting
+# Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+n1=input("enter first name: ")
+n2=input("enter last name: ")
+name=n1+" "+n2
+print("hello," +name+"! welcome to the python program")
